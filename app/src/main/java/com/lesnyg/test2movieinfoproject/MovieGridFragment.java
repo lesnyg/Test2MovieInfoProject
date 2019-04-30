cb1a74a864993a4ade532f93c39b458612090fc3
Junsuk Oh <a811219@gmail.com>
2019-04-30 16:55:10 +0900

    쓸데 없는 코드 삭제

--- app/src/main/java/com/lesnyg/test2movieinfoproject/MovieGridFragment.java
+++ app/src/main/java/com/lesnyg/test2movieinfoproject/MovieGridFragment.java
@@ -10,8 +10,6 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RatingBar;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -116,7 +114,6 @@ public class MovieGridFragment extends Fragment {
             public void onChanged(List<Result> results) {
                 mAdapter.setitems(results);
                 mRecycler.setAdapter(mAdapter);
-                mAdapter.notifyDataSetChanged();
                 mSwipeRefreshLayout.setRefreshing(false);
 
             }
